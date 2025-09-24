5ae620badb2d153e39203eafbcb07e33e28d61cd
Randy894 <randylukusa3@gmail.com>
2025-09-24 10:56:15 +0100

    Update main.js

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,11 @@
-// Exercice 5
-// Écrivez votre code ici
+// Déclarer une variable pour la température en Celsius
+
+let celsius = 25; // tu peux changer cette valeur
+
+// Convertir Celsius en Fahrenheit
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+// Afficher le résultat
+
+console.log(celsius + "°C équivaut à " + fahrenheit + "°F");
