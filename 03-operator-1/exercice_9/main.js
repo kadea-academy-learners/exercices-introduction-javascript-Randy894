6eb7a4177b7fcf3d3d916c6a19b10653dd25788e
Randy894 <randylukusa3@gmail.com>
2025-09-25 22:28:01 +0100

    feat:calculationof inheritance

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,2 +1,31 @@
-// Exercice 9
-// Écrivez votre code ici
+// Déclaration des montants de départ
+let a = 60000000 ;
+let b = 40000000 ;
+let c = 20000000 ;
+
+// Somme totale des montants
+let somme = a + b + c ;
+
+// Catégorie A représente 75% de la somme
+let categoriesA = somme / 100 * 75 ;
+
+// Répartition de la catégorie A en trois parts égales
+let Paul = categoriesA / 3 ;
+let Marie = categoriesA / 3 ;
+let EricetClaire = categoriesA / 3 / 2 ; // Chaque part de la troisième portion est divisée en deux
+
+// Catégorie B représente 25% de la somme
+let categoriesB = somme / 100 * 25 ;
+
+// Répartition de la catégorie B en trois parts égales
+let Mmemukuna = categoriesB / 3 ;
+let Joseph = categoriesB / 3 ;
+let Sarah = categoriesB / 3 ;
+
+// Affichage des résultats
+console.log("Paul reçoit : " + Paul)
+console.log("Marie reçoit : " +  Marie)
+console.log("Eric et Claire reçoivent chacun : " +  EricetClaire)
+console.log("Madame MUKUNA reçoit : " +  Mmemukuna)
+console.log("Joseph reçoit : " + Joseph)
+console.log("Sarah reçoit : " + Sarah)
