df4e9d3b58246d51f5a4e321d02f082b9995a794
Randy894 <randylukusa3@gmail.com>
2025-09-25 11:52:10 +0100

    declaration of values

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,16 +1,16 @@
-// 1. Variable numérique
+//  Variable numérique
 let age = 25;
 
-// 2. Variable chaîne de caractères
+//  Variable chaîne de caractères
 let nom = "Léonard";
 
-// 3. Variable booléenne
+//  Variable booléenne
 let estConnecte = true;
 
-// 4. Variable avec valeur null
+//  Variable avec valeur null
 let valeurNulle = null;
 
-// 5. Variable déclarée mais sans valeur
+//  Variable déclarée mais sans valeur
 let inconnue;
 
 // Affichage de toutes les variables
