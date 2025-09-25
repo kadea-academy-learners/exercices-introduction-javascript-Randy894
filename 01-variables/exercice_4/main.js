955e1ca6fc8d596ea690bc44483a85af7695de8e
Randy894 <randylukusa3@gmail.com>
2025-09-25 11:39:43 +0100

    calculating the sum

--- 01-variables/exercice_4/main.js
+++ 01-variables/exercice_4/main.js
@@ -1,9 +1,11 @@
-// calcule la somme
+// variable A && B
 
 let a =5;
 let b =8;
 
+// calcule de la somme
+
 let somme =a + b
 
 // affichage de la somme
-console.log(somme)
+console.log("La somme est de :" + somme)
