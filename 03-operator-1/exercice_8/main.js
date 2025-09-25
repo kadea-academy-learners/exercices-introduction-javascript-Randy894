14837caa9a61e48d91ad38fd875fae2c7eb97084
Randy894 <randylukusa3@gmail.com>
2025-09-25 09:47:40 +0100

    Update main.js

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,38 @@
-// Exercice 8
-// Écrivez votre code ici
+//
+
+let SalaireMensuel = 500 ;
+
+//calcule du Loyer
+
+let loyer = 0.30 * SalaireMensuel ;
+
+//calcule pour la Nourriture
+
+let Nourriture = 0.20 * SalaireMensuel ;
+
+//calcule du Transport
+
+let Transport = 0.10 * SalaireMensuel ;
+
+//valeur ajouter aux autres dépenses
+
+let Autresdepenses = 50 ;
+Autresdepenses += 25 ;
+
+//calcule de la somme
+
+let somme = loyer + Nourriture + Transport + Autresdepenses;
+
+//calcule du reste
+
+let reste =SalaireMensuel - somme ;
+
+//Affichage
+
+console.log("Le Salairemensuel est de :" + SalaireMensuel)
+console.log("Le loyer est de :" + loyer)
+console.log("La Nourriture est de :" + Nourriture)
+console.log("Le Transport est de :" + Transport)
+console.log("Les Autresdepenses sont de :" + Autresdepenses)
+console.log("La somme est de :" + somme)
+console.log("Le Reste est de :" + reste)
