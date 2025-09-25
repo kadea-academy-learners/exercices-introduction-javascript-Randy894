148a94e580ffb87a5c0e6c26a759b36f91262e47
Randy894 <randylukusa3@gmail.com>
2025-09-25 09:08:57 +0100

    Update main.js

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,25 @@
-// Exercice 7
-// Écrivez votre code ici
+// variable âgeEnfant
+
+let âgeEnfant = 20;
+
+// variable âgePère
+
+let âgePère = âgeEnfant *2 ;
+
+// variable âgeMère
+
+let âgeMère = âgePère - 5 ;
+
+// variable âgeGrandpère
+
+let âgeGrandpère = (âgeMère * 2) + (âgeEnfant / 2)
+
+// variable âgeOncle
+
+let âgeOncle = âgePère + 10 ;
+
+console.log("l'âge de l'Enfant est de :" + âgeEnfant)
+console.log("l'âge du père est de :" + âgePère)
+console.log("l'âge de la mère est de :" + âgeMère)
+console.log("l'âge du grang-père est de :" + âgeGrandpère)
+console.log("l'âge de l'oncle est de :" + âgeOncle)
